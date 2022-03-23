e51bfe1977e3136fec8a282a22e169ac3553c0de
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-03-22 23:50:45 -0300

    Ajusta los scopes de las funciones
    Para limitar la accesibilidad de las funciones se movieron las mismas a las funciones donde son llamadas para evitar que se encuentren dentro del "global" de la aplicacion

--- index.js
+++ index.js
@@ -1,152 +1,146 @@
 function memoria() {
-  const numeroPares = 8;
-  const nombresParejas = [
-    "Azul",
-    "Gris",
-    "Verde",
-    "Rojo",
-    "Amarillo",
-    "Celeste",
-    "Blanco",
-    "Negro",
-  ];
-  const coloresParejas = [
-    "bg-primary",
-    "bg-secondary",
-    "bg-success",
-    "bg-danger",
-    "bg-warning",
-    "bg-info",
-    "bg-light",
-    "bg-dark",
-  ];
+  //GLOBALES
   let IDcontador = 0;
   let puntaje = 0;
 
-
-  function resetearJuego() {
-    quitarCartas();
-    if (IDcontador) {
-      clearInterval(IDcontador);
-    }
-    puntaje = 0;
-    actualizarPuntaje(puntaje);
-  }
-
-function actualizarPuntaje(puntaje) {
-  document.querySelector("#puntaje").innerText = puntaje;
-}
-
-
-  function conseguirArrayAleatorio(numeroPares) {
-    const pares = [];
-    const arrayAleatorio = [];
-    let i = 0;
-    while (i < numeroPares) {
-      let posicion = Math.trunc(numeroPares * 2 * Math.random());
-      if (arrayAleatorio[posicion] === undefined) {
-        arrayAleatorio[posicion] = i;
-        if (pares[i] === "OK") {
-          i++;
-        } else {
-          pares[i] = "OK";
-        }
+  //FUNCIONES
+  function iniciarJuego() {
+    function resetearJuego() {
+      function quitarCartas() {
+        document.querySelector("#contenedor-cartas").innerHTML = "";
       }
-    }
-    return arrayAleatorio;
-  }
 
-  function configurarClickCarta(numeroDeCarta) {
-    document.querySelectorAll(".card").forEach((element) => {
-      element.onclick = numeroDeCarta;
-    });
-  }
-
-  function crearCarta() {
-    const $nuevaCarta = document.createElement("div");    
-    $nuevaCarta.className = "card border border-dark m-1 invisible";
-    $nuevaCarta.style = "width: 9rem";
-    return $nuevaCarta;
-  }
-
-  function conseguirNombreCarta(id) {
-    return nombresParejas[id];
-  }
-
-  function conseguirColorCarta(id) {
-    return coloresParejas[id];
-  }
-
-  function crearCuerpoCarta(element) {
-    const $cuerpoCarta = document.createElement("div");
-    $cuerpoCarta.className = "card-body invisible";
-    const $tituloCarta = document.createElement("h5");
-    $tituloCarta.className = "card-title text-center";
-    $tituloCarta.innerText = conseguirNombreCarta(element);
-    const $colorCarta = document.createElement("div");
-    $colorCarta.className = `${conseguirColorCarta(
-      element
-    )} img-thumbnail ratio ratio-4x3 border border-dark`;
-    $cuerpoCarta.appendChild($tituloCarta);
-    $cuerpoCarta.appendChild($colorCarta);
-    return $cuerpoCarta;
-  }
-
-  function voltearCarta(element) {
-    element.childNodes[0].classList.toggle("invisible");
-  }
-
-  function funcionPrimeraCarta(evento) {
-    const $carta1 = evento.srcElement;
-    voltearCarta($carta1);
-    configurarClickCarta((evento) => funcionSegundaCarta(evento, $carta1));
-
-    function funcionSegundaCarta(evento) {
-      const $carta2 = evento.srcElement;
-      voltearCarta($carta2);
-      if ($carta1.innerHTML === $carta2.innerHTML) {
-        setTimeout(() => {
-          $carta1.classList.add("invisible");
-          $carta2.classList.add("invisible");
-        }, 500);
-        puntaje++;
-        actualizarPuntaje(puntaje);
-      } else {
-        setTimeout(() => {
-          voltearCarta($carta1);
-          voltearCarta($carta2);
-        }, 500);
-      }
-      if (puntaje === 8) {
+      quitarCartas();
+      if (IDcontador) {
         clearInterval(IDcontador);
       }
-      configurarClickCarta(funcionPrimeraCarta);
+      puntaje = 0;
+      actualizarPuntaje(puntaje);
     }
-  }
-  function añadirCartas(arrayAleatorio) {
-    arrayAleatorio.forEach((element) => {
-      const $nuevaCarta = crearCarta();
-      const $cuerpoCarta = crearCuerpoCarta(element);
-      $nuevaCarta.appendChild($cuerpoCarta);
 
-      const $Contenedor = document.querySelector("#contenedor-cartas");
-      $Contenedor.appendChild($nuevaCarta);
-    });
-  }
+    function actualizarPuntaje(puntaje) {
+      document.querySelector("#puntaje").innerText = puntaje;
+    }
 
-  function mostrarCartas() {
-    document.querySelectorAll(".card").forEach((element) => {
-      element.classList.remove("invisible");
-    });
-  }
+    function añadirCartas(arrayAleatorio) {
+      arrayAleatorio.forEach((element) => {
+        const $nuevaCarta = crearCarta();
+        const $cuerpoCarta = crearCuerpoCarta(element);
+        $nuevaCarta.appendChild($cuerpoCarta);
+        const $Contenedor = document.querySelector("#contenedor-cartas");
+        $Contenedor.appendChild($nuevaCarta);
+      });
+      function crearCarta() {
+        const $nuevaCarta = document.createElement("div");
+        $nuevaCarta.className = "card border border-dark m-1 invisible";
+        $nuevaCarta.style = "width: 9rem";
+        return $nuevaCarta;
+      }
+      function crearCuerpoCarta(element) {
+        function conseguirNombreCarta(id) {
+          const nombresParejas = [
+            "Azul",
+            "Gris",
+            "Verde",
+            "Rojo",
+            "Amarillo",
+            "Celeste",
+            "Blanco",
+            "Negro",
+          ];
+          return nombresParejas[id];
+        }
+        function conseguirColorCarta(id) {
+          const coloresParejas = [
+            "bg-primary",
+            "bg-secondary",
+            "bg-success",
+            "bg-danger",
+            "bg-warning",
+            "bg-info",
+            "bg-light",
+            "bg-dark",
+          ];
+          return coloresParejas[id];
+        }
 
-  function quitarCartas() {
-    document.querySelector("#contenedor-cartas").innerHTML = "";
-  }
+        const $cuerpoCarta = document.createElement("div");
+        $cuerpoCarta.className = "card-body invisible";
+        const $tituloCarta = document.createElement("h5");
+        $tituloCarta.className = "card-title text-center";
+        $tituloCarta.innerText = conseguirNombreCarta(element);
+        const $colorCarta = document.createElement("div");
+        $colorCarta.className = `${conseguirColorCarta(
+          element
+        )} img-thumbnail ratio ratio-4x3 border border-dark`;
+        $cuerpoCarta.appendChild($tituloCarta);
+        $cuerpoCarta.appendChild($colorCarta);
+        return $cuerpoCarta;
+      }
+    }
+    function conseguirArrayAleatorio() {
+      const numeroPares = 8;
+      const pares = [];
+      const arrayAleatorio = [];
+      let i = 0;
+      while (i < numeroPares) {
+        let posicion = Math.trunc(numeroPares * 2 * Math.random());
+        if (arrayAleatorio[posicion] === undefined) {
+          arrayAleatorio[posicion] = i;
+          if (pares[i] === "OK") {
+            i++;
+          } else {
+            pares[i] = "OK";
+          }
+        }
+      }
+      return arrayAleatorio;
+    }
 
-  function iniciarJuego() {
+    function configurarClickCarta(numeroDeCarta) {
+      document.querySelectorAll(".card").forEach((element) => {
+        element.onclick = numeroDeCarta;
+      });
+    }
+    function funcionPrimeraCarta(evento) {
+      function voltearCarta(element) {
+        element.childNodes[0].classList.toggle("invisible");
+      }
+
+      const $carta1 = evento.srcElement;
+      voltearCarta($carta1);
+      configurarClickCarta((evento) => funcionSegundaCarta(evento, $carta1));
+
+      function funcionSegundaCarta(evento) {
+        const $carta2 = evento.srcElement;
+        voltearCarta($carta2);
+        if ($carta1.innerHTML === $carta2.innerHTML) {
+          setTimeout(() => {
+            $carta1.classList.add("invisible");
+            $carta2.classList.add("invisible");
+          }, 500);
+          puntaje++;
+          actualizarPuntaje(puntaje);
+        } else {
+          setTimeout(() => {
+            voltearCarta($carta1);
+            voltearCarta($carta2);
+          }, 500);
+        }
+        if (puntaje === 8) {
+          clearInterval(IDcontador);
+        }
+        configurarClickCarta(funcionPrimeraCarta);
+      }
+    }
+    function mostrarCartas() {
+      document.querySelectorAll(".card").forEach((element) => {
+        element.classList.remove("invisible");
+      });
+    }
+
     resetearJuego();
-    añadirCartas(conseguirArrayAleatorio(numeroPares));
+    añadirCartas(conseguirArrayAleatorio());
     configurarClickCarta(funcionPrimeraCarta);
     mostrarCartas();
     let contador = 0;
@@ -158,10 +152,9 @@ function actualizarPuntaje(puntaje) {
         document.querySelector("#contador").innerText = contador;
       }, 100);
     }
-    
   }
 
-  //añadirCartas(conseguirArrayAleatorio(numeroPares));
+  //INICIO
   document.querySelector("#start").onclick = iniciarJuego;
 }
 
